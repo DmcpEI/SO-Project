a0ee6d3e673e368428b55e1ca60d83d163557f4f
Roberto-Andrade-1 <2065421@student.uma.pt>
2023-10-26 00:48:03 +0100

    simulador feito

--- simulador.c
+++ simulador.c
@@ -3,39 +3,38 @@
 
 int main(int argc, char* argv[]){
 
-    struct configuracao conf;
-    
-    if(argc != 2){
-	printf("É preciso passar como argumento um ficheiro de configuração.\n");
+    if(argc == 2){
+
+	struct configuracao conf;
+	FILE *ficheiro = fopen(argv[1], "r");
+
+	if(ficheiro == NULL){
+	    perror("Erro ao abrir o ficheiro");
+	    return 1;
+	}else if (strcmp(argv[1], "simulador.conf") != 0){
+	perror("Ficheiro de configuração não existe.");
 	return 1;
-    }
-
-    FILE *ficheiro = fopen(argv[1], "r");
-
-    if(ficheiro == NULL){
-	perror("Erro ao abrir o ficheiro");
-	return 1;
-    }else{
-	
-	fseek(ficheiro, 0, SEEK_END);
-	long tamanhoFicheiro = ftell(ficheiro);
-	rewind(ficheiro);
-	char buffer[tamanhoFicheiro];
-	fread(buffer, 1, tamanhoFicheiro, ficheiro);
-	fclose(ficheiro);
-
-	int linhaAtual = 0;
-	char *filtrada = strtok(buffer, "\n");
-	char *linhas[30];
-	while(filtrada != NULL){
-	    linhas[linhaAtual++] = filtrada;
-	    filtrada = strtok(NULL, "\n");
 	}
 
+	fseek(ficheiro, 0, SEEK_END);
+        long tamanhoFicheiro = ftell(ficheiro);
+        rewind(ficheiro);
+        char buffer[tamanhoFicheiro];
+        fread(buffer, 1, tamanhoFicheiro, ficheiro);
+        fclose(ficheiro);
+
+        int linhaAtual = 0;
+        char *filtrada = strtok(buffer, "\n");
+        char *linhas[40];
+        while(filtrada != NULL){
+	    linhas[linhaAtual++] = filtrada;
+	    filtrada = strtok(NULL, "\n");
+        }
+
 	char *fim;
-	char *array[2];
-	char *valores[16];
-	for(int index = 0; index < 12; index++){
+        char *array[2];
+	char *valores[17];
+	for(int index = 0; index < 17; index++){
 	    char *aux = strtok(linhas[index], ":");
 	    int i = 0;
 	    while (aux != NULL){
@@ -46,25 +45,40 @@ int main(int argc, char* argv[]){
 	}
     
 	conf.quantidadePessoasParque = atoi(valores[0]);
-	conf.tempoEsperaEntrada = atoi(valores[1]);
-	conf.numeroAtracoes = atoi(valores[2]);
-	conf.tempoEsperaAtracao1 = atoi(valores[3]);
-	conf.tempoEsperaAtracao2 = atoi(valores[4]);
-	conf.tempoEsperaAtracao3 = atoi(valores[5]);
-	conf.tempoEsperaAtracao4 = atoi(valores[6]);
-	conf.tempoEsperaAtracao5 = atoi(valores[7]); 
-	conf.tamanhoFilaAtracao1 = atoi(valores[8]); 
-	conf.tamanhoFilaAtracao2 = atoi(valores[9]); 
-	conf.tamanhoFilaAtracao3 = atoi(valores[10]); 
-	conf.tamanhoFilaAtracao4 = atoi(valores[11]); 
-	conf.tamanhoFilaAtracao5 = atoi(valores[12]);
-	conf.probabilidadeMagoar = strtof(valores[13], &fim, 10);
-	conf.probabilidadeDesistir = strtof(valores[14], &fim, 10);
-	conf.tempoSimulacao = atoi(valores[15]);
-
-	printf("Quantidade de pessoas no parque: %d\n", conf.quantidadePessoasParque);
-	
-
+        conf.tempoEsperaEntrada = atoi(valores[1]);
+        conf.numeroAtracoes = atoi(valores[2]);
+        conf.tempoEsperaAtracao1 = atoi(valores[3]);
+        conf.tempoEsperaAtracao2 = atoi(valores[4]);
+        conf.tempoEsperaAtracao3 = atoi(valores[5]);
+        conf.tempoEsperaAtracao4 = atoi(valores[6]);
+        conf.tempoEsperaAtracao5 = atoi(valores[7]); 
+        conf.tamanhoFilaAtracao1 = atoi(valores[8]); 
+        conf.tamanhoFilaAtracao2 = atoi(valores[9]); 
+        conf.tamanhoFilaAtracao3 = atoi(valores[10]); 
+        conf.tamanhoFilaAtracao4 = atoi(valores[11]); 
+        conf.tamanhoFilaAtracao5 = atoi(valores[12]);
+        conf.probabilidadeMagoar = strtof(valores[13], &fim);
+        conf.probabilidadeDesistir = strtof(valores[14], &fim);
+        conf.tempoSimulacao = atoi(valores[15]);
+    
+        printf("Quantidade de pessoas no parque: %d\n", conf.quantidadePessoasParque);
+        printf("Tempo de espera na entrada: %d\n", conf.tempoEsperaEntrada);
+        printf("Número de atrações: %d\n", conf.numeroAtracoes);
+        printf("Tempo de espera atracao 1: %d\n", conf.tempoEsperaAtracao1);
+        printf("Tempo de espera atracao 2: %d\n", conf.tempoEsperaAtracao2);
+        printf("Tempo de espera atracao 3: %d\n", conf.tempoEsperaAtracao3);
+        printf("Tempo de espera atracao 4: %d\n", conf.tempoEsperaAtracao4);
+        printf("Tempo de espera atracao 5: %d\n", conf.tempoEsperaAtracao5);
+        printf("Tamanho da fila atracao 1: %d\n", conf.tamanhoFilaAtracao1);
+        printf("Tamanho da fila atração 2: %d\n", conf.tamanhoFilaAtracao2);
+        printf("Tamanho da fila atração 3: %d\n", conf.tamanhoFilaAtracao3);
+        printf("Tamanho da fila atração 4: %d\n", conf.tamanhoFilaAtracao4);
+        printf("Tamanho da fila atração 5: %d\n", conf.tamanhoFilaAtracao5);
+        printf("Probabilidade de se magoar: %f\n", conf.probabilidadeMagoar);
+        printf("Probabilidade de desistir: %f\n", conf.probabilidadeDesistir);
+        printf("Tempo de Simulação: %d\n", conf.tempoSimulacao);
 	return 0;
-    }
+	}
+	perror("É preciso como argumento o ficheiro de configuração.");
+	return 1;
 }

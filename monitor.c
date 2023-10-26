1ec6085274dae892277c6d4ff8d16eb11549a290
DmcpEI <diogomcpaixao@gmail.com>
2023-10-26 10:24:38 +0100

    Monitor update

--- monitor.c
+++ monitor.c
@@ -6,30 +6,37 @@
 int main(int argc, char *argv[]) {
     // Verifica se o número de argumentos é válido
     if (argc != 3) {
-        printf("Uso: %s <ficheiro> <texto>\n", argv[0]);
+        printf("Deve escrever como argumentos: %s <ficheiro> <texto>\n", argv[0]);
         return 1;
     }
 
     char *nomeFicheiro = argv[1];
     char *texto = argv[2];
 
-    // Abre o ficheiro no modo de escrita (acrescentar)
+    // Verifica se o arquivo já existe
+    FILE *verificador = fopen(nomeFicheiro, "r");
+    
+    if (!verificador) {
+        printf("Criou ficheiro %s.\n", nomeFicheiro);
+    }
+
+    // Abre o arquivo no modo de acrescentar ou criar
     FILE *ficheiro = fopen(nomeFicheiro, "a");
 
-    // Verifica se o ficheiro foi aberto com sucesso
+    // Verifica se o arquivo foi aberto com sucesso
     if (ficheiro == NULL) {
         printf("Erro ao abrir o ficheiro.\n");
         return 1;
     }
+    else {
+        // Escreve o texto no arquivo
+        fprintf(ficheiro, "%s\n", texto);
 
-    // Escreve o texto no ficheiro
-    fprintf(ficheiro, "%s\n", texto);
+        // Fecha o arquivo
+        fclose(ficheiro);
 
-    // Fecha o ficheiro
-    fclose(ficheiro);
+        printf("Texto adicionado com sucesso em %s.\n", nomeFicheiro);
 
-    printf("Texto adicionado com sucesso!\n");
-
-    return 0;
+        return 0;
+    }
 }
-

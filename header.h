32b02d7c59edcdb6e08513281dbcd2e4a6c30027
DmcpEI <diogomcpaixao@gmail.com>
2023-11-10 17:04:26 +0000

    Structs e Atrações

--- header.h
+++ header.h
@@ -13,16 +13,6 @@
 #include <stdbool.h>
 #include <semaphore.h>
 
-extern int fd;
-extern char *ptr;
-extern int nbytes;
-extern int readn(int fd, char *ptr, int nbytes);
-extern int writen(int fd, char *ptr, int nbytes);
-extern int readline(int fd, char *ptr, int maxlen);
-extern void err_dump(char *msg);
-extern void str_cli(FILE *fp, int sockfd);
-extern void str_echo(int sockfd);
-
 #define UNIXSTR_PATH "/tmp/s.unixstr"
 #define UNIXDG_PATH  "/tmp/s.unixdgx"
 #define UNIXDG_TMP   "/tmp/dgXXXXXXX"
@@ -30,33 +20,46 @@ extern void str_echo(int sockfd);
 struct configuracao{
 	
 	int quantidadePessoasParque;
-	int tempoEsperaEntrada;
 	int numeroAtracoes;
-	int tempoEsperaAtracao1;
-	int tempoEsperaAtracao2;
-	int tempoEsperaAtracao3;
-	int tempoEsperaAtracao4;
-	int tempoEsperaAtracao5; 
-	int tamanhoFilaAtracao1; 
-	int tamanhoFilaAtracao2; 
-	int tamanhoFilaAtracao3; 
-	int tamanhoFilaAtracao4; 
-	int tamanhoFilaAtracao5;
+	int tempoEsperaBilheteria;
+	int tempoEsperaNatação;
+	int tamanhoFilaNatação;	
+	int tempoEsperaMergulho;
+	int tamanhoFilaMergulho;
+	int tempoEsperaTobogãs;
+	int tamanhoFilaTobogãs;	
+	int tempoEsperaEnfermaria;
+	int tamanhoFilaEnfermaria;	
+	int tempoEsperaRestauração;
+	int tamanhoFilaRestauração;
+	int tempoEsperaBalnearios;
+	int tamanhoFilaBalnearios;
+	//int tempoEsperaParqueEstacionamento;
+	//int tamanhoFilaParqueEstacionamento;
 	float probabilidadeMagoar;
-	float probabilidadeDesistir;
+	float probabilidadeDesistir; //Não devia de estar em pessoa?
+	//float probabilidadeMembro (Probabilidade der ser membro do parque e passar sempre a frente nas filas, pode ser uma boa politica)
 	int tempoSimulacao;
 };
 
+// Bilheteria
+// Natação
+// Mergulho
+// Tobogãs
+// Enfermaria
+// Restauração
+// Balnearios
+// Parque de estacionamento(talvez)
+struct zona {
 
-struct atracao {
-
-	int idAtracao; //Id da atração
-	int numeroMaximoPessoas; //Número máximo de pessoas na atração
-	int numeroAtualPessoas; //Número atual de pessoas na atração
-	int idadeMinima; //Idade mínima para aceder à atração
-	int alturaMinima; //Altura mínima para aceder à atração
-	int tempoMaxAtracao;
-	float probMagoar; //Probabilidade de se magoar na atração
+	int idZona; //Id da zona (Começa em 1)
+	int numeroMaximoPessoas; //Número máximo de pessoas na zona
+	int numeroAtualPessoas; //Número atual de pessoas na zona
+	int idadeMinima; //Idade mínima para aceder à zona
+	int alturaMinima; //Altura mínima para aceder à zona
+	int tempoMaxZona;
+	float probMagoar; //Probabilidade de se magoar na zona
+	//sem_t fila //Fila de espera da zona 
 };
 
 struct pessoa {
@@ -68,7 +71,29 @@ struct pessoa {
 	int onParque; //Se está no parque (0 - Não está / 1 - Está)
 	int vip; //Se é VIP do parque ou não (passa à frente) (0 - Não é VIP / 1 - É VIP)
 	int magoar; //Se se magoou ou não (0 - Não se magoou / 1 - Magoou-se)
-	int atracaoAtual; //Id da atração atual (0 - Não está em nenhuma atração / ...)
+	int zonaAtual; //Id da atração atual (0 - Não está em nenhuma atração / ...)
 	int tempoMaxEspera; //Tempo máximo de espera numa fila (em ciclos)
 
 };
+
+extern int fd;
+extern char *ptr;
+extern int nbytes;
+extern int readn(int fd, char *ptr, int nbytes);
+extern int writen(int fd, char *ptr, int nbytes);
+extern int readline(int fd, char *ptr, int maxlen);
+extern void err_dump(char *msg);
+extern void str_cli(FILE *fp, int sockfd);
+extern void str_echo(int sockfd);
+
+//Métodos
+
+//Simulador
+void socketSimulador();
+struct pessoa criarPessoa();
+void enviarDados(char* dados);
+
+//Monitor
+void socketMonitor();
+void escreveDados(char* dados);
+
